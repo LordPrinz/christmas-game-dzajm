99177b2d5095165b9c9bb65c8f7f33293c29b077
LordPrinz <plazinski2902@gmail.com>
2023-11-11 18:21:47 +0100

    Install tailwind utils

--- src/app/layout/Navigation/Navigation.tsx
+++ src/app/layout/Navigation/Navigation.tsx
@@ -1,10 +1,13 @@
 import Logo from "@/app/components/shared/Logo";
+import Link from "next/link";
 import React from "react";
 
 const Navigation = () => {
 	return (
 		<nav className="top-0 fixed w-full ">
-			<Logo />
+			<Link href="/" className="max-h-[5px]">
+				<Logo />
+			</Link>
 		</nav>
 	);
 };
