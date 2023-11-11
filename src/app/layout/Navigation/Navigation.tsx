951f23cdb161bcfe184019698917796c1dcd6bf5
LordPrinz <plazinski2902@gmail.com>
2023-11-11 18:17:34 +0100

    Add Logo

--- src/app/layout/Navigation/Navigation.tsx
+++ src/app/layout/Navigation/Navigation.tsx
@@ -1,7 +1,12 @@
+import Logo from "@/app/components/shared/Logo";
 import React from "react";
 
 const Navigation = () => {
-	return <nav className="top-0 fixed w-full "></nav>;
+	return (
+		<nav className="top-0 fixed w-full ">
+			<Logo />
+		</nav>
+	);
 };
 
 export default Navigation;
