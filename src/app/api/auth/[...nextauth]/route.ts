412d4d40855a7ea65251bbb86e6038753d099376
LordPrinz <plazinski2902@gmail.com>
2023-11-12 18:29:01 +0100

    Add NextAuth provider

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth/next";
+import AzureADProvider from "next-auth/providers/azure-ad";
+
+const handler = NextAuth({
+	providers: [
+		AzureADProvider({
+			clientId: process.env.AZURE_AD_CLIENT_ID,
+			clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
+			tenantId: process.env.AZURE_AD_TENANT_ID,
+		}),
+	],
+});
+
+export { handler as GET, handler as POST };
