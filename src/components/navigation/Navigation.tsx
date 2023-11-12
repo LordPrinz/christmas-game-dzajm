bbd5019f86b5cc1f5c3e971d246b76fec0cdeea5
LordPrinz <plazinski2902@gmail.com>
2023-11-12 21:33:02 +0100

    Style navigation

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -4,14 +4,22 @@ import SignOut from "./auth/SignOut";
 
 const Navigation = () => {
 	return (
-		<nav className="top-0 fixed w-full my-8 px-8 flex items-center justify-between">
+		<nav className="top-7 absolute w-full  px-8 flex items-center justify-between">
 			<div className="flex items-center gap-9">
 				<Link href="/">
-					<Logo className="h-8" />
+					<Logo className="h-8 transition  fill-theme-twilight-blue-50 hover:fill-theme-twilight-blue-200 hover:transition" />
 				</Link>
-				<div className="flex gap-7 text-theme-twilight-blue-50 text-lg">
-					<Link href="/">Strona Główna</Link>
-					<Link href="https://dzaj.de/christmas">Zapisz się</Link>
+				<div className="flex gap-3 text-theme-twilight-blue-50 text-lg">
+					<Link
+						className="py-2 hover:text-theme-twilight-blue-200 px-2 transition hover:transition"
+						href="https://dzaj.de/christmas_reg">
+						Regulamin
+					</Link>
+					<Link
+						className="py-2 hover:text-theme-twilight-blue-200 px-2 transition hover:transition"
+						href="https://dzaj.de/christmas">
+						Zapisz się
+					</Link>
 				</div>
 			</div>
 			<div>
--- src/components/navigation/auth/SignOut.tsx
+++ src/components/navigation/auth/SignOut.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { signOut } from "next-auth/react";
+import { ArrowRightOnRectangleIcon } from "@heroicons/react/24/outline";
 
 const SignOut = () => {
 	const handleLogout = async () => {
@@ -10,7 +11,14 @@ const SignOut = () => {
 		});
 	};
 
-	return <div onClick={handleLogout}>leave</div>;
+	return (
+		<div
+			onClick={handleLogout}
+			className="cursor-pointer hover:bg-theme-twilight-blue-700 bg-theme-twilight-blue-600 py-2.5 transition hover:transition rounded-full flex gap-1.5 items-center px-4">
+			<span className="text-theme-snow text-sm">Wyloguj</span>
+			<ArrowRightOnRectangleIcon className="h-6 text-theme-snow" />
+		</div>
+	);
 };
 
 export default SignOut;
