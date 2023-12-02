24d005a271a5f2b1dde552ae5a9924ddd0d1f3b5
LordPrinz <plazinski2902@gmail.com>
2023-12-02 15:12:19 +0100

    Add responsive to navbar

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -4,8 +4,8 @@ import SignOut from "./auth/SignOut";
 
 const Navigation = () => {
 	return (
-		<nav className="top-7 absolute w-full  px-8 flex items-center justify-between z-20 ">
-			<div className="flex items-center gap-9">
+		<nav className="top-5 sm:top-7 absolute w-full px-3 sm:px-8 flex items-center justify-between z-20 ">
+			<div className="flex items-center gap-5 sm:gap-9">
 				<Link href="/">
 					<Logo className="h-6 sm:h-8 transition  fill-theme-twilight-blue-50 hover:fill-theme-twilight-blue-200 hover:transition" />
 				</Link>
