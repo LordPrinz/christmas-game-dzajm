100ef45e27709b70d5691397d4b7fed5a720a86d
LordPrinz <plazinski2902@gmail.com>
2023-12-02 15:04:49 +0100

    Add responsive to navigation

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -7,16 +7,16 @@ const Navigation = () => {
 		<nav className="top-7 absolute w-full  px-8 flex items-center justify-between z-20 ">
 			<div className="flex items-center gap-9">
 				<Link href="/">
-					<Logo className="h-8 transition  fill-theme-twilight-blue-50 hover:fill-theme-twilight-blue-200 hover:transition" />
+					<Logo className="h-6 sm:h-8 transition  fill-theme-twilight-blue-50 hover:fill-theme-twilight-blue-200 hover:transition" />
 				</Link>
-				<div className="flex gap-3 text-theme-twilight-blue-50 text-lg">
+				<div className="flex gap-2 sm:gap-3 text-theme-twilight-blue-50 text-lg">
 					<Link
-						className="py-2 hover:text-theme-twilight-blue-200 px-2 transition hover:transition"
+						className="py-2  hover:text-theme-twilight-blue-200 px-1.5 sm:px-2 transition hover:transition text-sm sm:text-base"
 						href="https://dzaj.de/christmas_reg">
 						Regulamin
 					</Link>
 					<Link
-						className="py-2 hover:text-theme-twilight-blue-200 px-2 transition hover:transition"
+						className="py-2 hover:text-theme-twilight-blue-200 px-1.5 sm:px-2 transition hover:transition text-sm sm:text-base"
 						href="https://dzaj.de/christmas">
 						Zapisz się
 					</Link>
