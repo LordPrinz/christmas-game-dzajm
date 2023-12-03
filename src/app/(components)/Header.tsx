bba6696cbb477119edd47083dc0664e4886ef758
LordPrinz <plazinski2902@gmail.com>
2023-12-03 13:23:13 +0100

    Make Header smaller

--- src/app/(components)/Header.tsx
+++ src/app/(components)/Header.tsx
@@ -8,10 +8,10 @@ const Header = () => {
 		<div className="w-full  col-[full-start/full-end]  flex flex-col bg-theme-twilight-blue-500 overflow-hidden">
 			<div className="flex flex-col items-center justify-center flex-1">
 				<div className="flex flex-col items-center justify-center">
-					<h1 className="text-4xl sm:text-6xl font-bold text-center text-theme-snow ">
+					<h1 className="text-3xl sm:text-6xl font-bold text-center text-theme-snow ">
 						Christmas Game Dzajm
 					</h1>
-					<h2 className="text-lg sm:text-2xl font-bold text-center text-theme-snow text-opacity-80">
+					<h2 className="text-base sm:text-2xl font-bold text-center text-theme-snow text-opacity-80">
 						Winter ZSTI game developers tournament
 					</h2>
 				</div>
